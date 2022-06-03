27acc61af3a75aca906d0d6101dd09666cdee866
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-03 17:56:50 +0600

    homepage layout added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,19 @@
 import React from "react";
 import JobOpenings from "../../Components/JobOpenings/JobOpenings";
 import Profile from "../../Components/Profile/Profile";
-import './Home.css';
+import "./Home.css";
 
 const Home = () => {
     return (
         <div className="openings">
             <h2 className="text-center mt-3 title">Job Openings</h2>
-            <div>
-                <Profile></Profile>
-            </div>
-            <div>
-                <JobOpenings></JobOpenings>
+            <div className="row g-5 mt-1">
+                <div className="col col-lg-4 d-flex justify-content-center">
+                    <Profile></Profile>
+                </div>
+                <div className="col col-lg-8">
+                    <JobOpenings></JobOpenings>
+                </div>
             </div>
         </div>
     );
