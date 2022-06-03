aadf0812a5980319b9b0811cd81324464981be00
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-03 17:07:44 +0600

    successfully fetched user profile data

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Profile = () => {
+    const [profile, setProfile] = useState({});
+    useEffect(() => {
+        fetch("http://refertest.pythonanywhere.com/user/data", {
+            method: "GET",
+            uid: 136,
+        })
+            .then((res) => res.json())
+            .then((data) => setProfile(data));
+    }, []);
+    
     return (
         <div>
             <h3>Profile</h3>
