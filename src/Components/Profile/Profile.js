d7070a40d9082a1289ddcae7598ff02e754d59ab
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-03 18:03:06 +0600

    added loading spinner

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
+import Loading from "../Loading/Loading";
 import "./Profile.css";
 
 const Profile = () => {
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
+        setLoading(true);
         fetch("http://refertest.pythonanywhere.com/user/data", {
             method: "GET",
             uid: 136,
         })
             .then((res) => res.json())
-            .then(({ data }) => setProfile(data));
+            .then(({ data }) => {
+                setProfile(data);
+                setLoading(false);
+            });
     }, []);
+
+    if (loading) {
+        return <Loading></Loading>;
+    }
 
     console.log(profile);
 
